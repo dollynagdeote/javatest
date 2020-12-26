a6d27e1c7b6c2c85f8e4217d89a21a9722696e85
dollynagdeote <76435708+dollynagdeote@users.noreply.github.com>
2020-12-26 17:47:01 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
 import java.lang.*;
-Public class HelloWorld {
+// Your First Program
+
+class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
     }
